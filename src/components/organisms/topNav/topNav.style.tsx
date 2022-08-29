88053a5a1e8fb44b95655278071ae44a38be3e20
Tony Sanjuan <tonysanj13@gmail.com>
2022-08-29 19:33:47 +0200

    feat: topnav modified

--- src/components/organisms/topNav/topNav.style.tsx
+++ src/components/organisms/topNav/topNav.style.tsx
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
 
 const SCTopNav = styled.div`
+    height: 100%;
+    
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--app-primary-color);
+    
+    .TopNav_MainContainer {
+        width: var(--app-width);
+        height: 100%;
+        
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
 `;
 
 export default SCTopNav;
--- src/components/organisms/topNav/topNav.tsx
+++ src/components/organisms/topNav/topNav.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Button from '../../atoms/button/button';
 import SCTopNav from './topNav.style';
 
 function TopNav() {
   return (
     <SCTopNav>
-      <p>header</p>
+      <div className="TopNav_MainContainer">
+        <Button text="Home" size="full" noBorder />
+        <Button text="Buttons" size="full" noBorder />
+        <Button text="Inputs" size="full" noBorder />
+        <Button text="Tooltip" size="full" noBorder />
+        <Button text="Forms" size="full" noBorder />
+        <Button text="Tables" size="full" noBorder />
+      </div>
     </SCTopNav>
   );
 }
