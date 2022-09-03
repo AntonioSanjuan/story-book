8962238259a11ce981ee9303278051eea0acbeae
Tony Sanjuan <tonysanj13@gmail.com>
2022-09-03 22:12:02 +0200

    feature: has been added some examples of navigation

--- src/components/organisms/topNav/topNav.tsx
+++ src/components/organisms/topNav/topNav.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../atoms/button/button';
 import SCTopNav from './topNav.style';
 
 function TopNav() {
+  const navigate = useNavigate();
+
   return (
     <SCTopNav>
       <div className="TopNav_MainContainer">
-        <Button text="Home" size="full" noBorder />
+        <Button text="Home" size="full" noBorder onClick={() => navigate('/')} />
         <Button text="Buttons" size="full" noBorder />
         <Button text="Inputs" size="full" noBorder />
         <Button text="Tooltip" size="full" noBorder />
         <Button text="Forms" size="full" noBorder />
-        <Button text="Tables" size="full" noBorder />
+        <Button text="Tables" size="full" noBorder onClick={() => navigate('/tables')} />
       </div>
     </SCTopNav>
   );
